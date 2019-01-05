166a9147e997c457466511be69f8bb0ccd3587a8
Guoxiao Cai <a304775438@aliyun.com>
2019-01-05 10:43:40 +0800

    fixup: demo Dropdown show margin bottom

--- examples/components/components/Dropdown.vue
+++ examples/components/components/Dropdown.vue
@@ -21,70 +21,67 @@
 
     <example-section-item title="Hoverable or Toggable">
       <demo>
-        <vb-buttons-list>
-          <vb-dropdown trigger="click">
-            <vb-button>
-              <span>Click me</span>
-              <vb-icon name="fa fa-angle-down"></vb-icon>
-            </vb-button>
-            <vb-dropdown-menu>
-              <vb-dropdown-item>Dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <hr class="dropdown-divider">
-              <vb-dropdown-item>With a divider</vb-dropdown-item>
-            </vb-dropdown-menu>
-          </vb-dropdown>
+        <vb-dropdown trigger="click">
+          <vb-button>
+            <span>Click me</span>
+            <vb-icon name="fa fa-angle-down"></vb-icon>
+          </vb-button>
 
-          <vb-dropdown>
-            <vb-button>
-              <span>Hover me</span>
-              <vb-icon name="fa fa-angle-down"></vb-icon>
-            </vb-button>
-            <vb-dropdown-menu>
-              <vb-dropdown-item>Dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <hr class="dropdown-divider">
-              <vb-dropdown-item>With a divider</vb-dropdown-item>
-            </vb-dropdown-menu>
-          </vb-dropdown>
-        </vb-buttons-list>
+          <vb-dropdown-menu>
+            <vb-dropdown-item>Dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <hr class="dropdown-divider">
+            <vb-dropdown-item>With a divider</vb-dropdown-item>
+          </vb-dropdown-menu>
+        </vb-dropdown>
+
+        <vb-dropdown>
+          <vb-button>
+            <span>Hover me</span>
+            <vb-icon name="fa fa-angle-down"></vb-icon>
+          </vb-button>
+          <vb-dropdown-menu>
+            <vb-dropdown-item>Dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <hr class="dropdown-divider">
+            <vb-dropdown-item>With a divider</vb-dropdown-item>
+          </vb-dropdown-menu>
+        </vb-dropdown>
         <pre slot="code">{{ toggable }}</pre>
       </demo>
     </example-section-item>
 
     <example-section-item title="Right aligned">
       <demo>
-        <vb-buttons-list>
-          <vb-dropdown right-aligned>
-            <vb-button>
-              <span>Right aligned</span>
-              <vb-icon name="fa fa-angle-down"></vb-icon>
-            </vb-button>
-            <vb-dropdown-menu>
-              <vb-dropdown-item>Dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <hr class="dropdown-divider">
-              <vb-dropdown-item>With a divider</vb-dropdown-item>
-            </vb-dropdown-menu>
-          </vb-dropdown>
+        <vb-dropdown right-aligned>
+          <vb-button>
+            <span>Right aligned</span>
+            <vb-icon name="fa fa-angle-down"></vb-icon>
+          </vb-button>
+          <vb-dropdown-menu>
+            <vb-dropdown-item>Dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <hr class="dropdown-divider">
+            <vb-dropdown-item>With a divider</vb-dropdown-item>
+          </vb-dropdown-menu>
+        </vb-dropdown>
 
-          <vb-dropdown>
-            <vb-button>
-              <span>Normal</span>
-              <vb-icon name="fa fa-angle-down"></vb-icon>
-            </vb-button>
-            <vb-dropdown-menu>
-              <vb-dropdown-item>Dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
-              <hr class="dropdown-divider">
-              <vb-dropdown-item>With a divider</vb-dropdown-item>
-            </vb-dropdown-menu>
-          </vb-dropdown>
-        </vb-buttons-list>
+        <vb-dropdown>
+          <vb-button>
+            <span>Normal</span>
+            <vb-icon name="fa fa-angle-down"></vb-icon>
+          </vb-button>
+          <vb-dropdown-menu>
+            <vb-dropdown-item>Dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <vb-dropdown-item>Other dropdown item</vb-dropdown-item>
+            <hr class="dropdown-divider">
+            <vb-dropdown-item>With a divider</vb-dropdown-item>
+          </vb-dropdown-menu>
+        </vb-dropdown>
         <pre slot="code">{{ aligned }}</pre>
       </demo>
     </example-section-item>
@@ -125,3 +122,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.dropdown:not(:last-child) {
+  margin-right: 0.5rem;
+}
+</style>
