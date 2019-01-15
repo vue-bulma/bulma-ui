2c0386cf52c941844f08e052a2b1ae1f40a1b8fc
Guoxiao Cai <a304775438@aliyun.com>
2019-01-15 10:42:39 +0800

    fixup: 1. Improve basic demo 2. Split sizes demo to tittle and subtitle

--- examples/code/elements/title.js
+++ examples/code/elements/title.js
@@ -1,25 +1,30 @@
 let code = {}
 code.basic = `\
-<vb-title>Title</vb-title>
+<vb-title>Title (default type)</vb-title>
 
 <vb-title type="subtitle">Subtitle</vb-title>
 `
 
-code.size = `\
-<vb-title size="1">Title</vb-title>
-<vb-title size="2">Title</vb-title>
-<vb-title size="3">Title</vb-title>
-<vb-title size="4">Title</vb-title>
-<vb-title size="5">Title</vb-title>
-<vb-title size="6">Title</vb-title>
+code.size = {}
 
-<vb-title type="subtitle" size="1">Subtitle</vb-title>
-<vb-title type="subtitle" size="2">Subtitle</vb-title>
-<vb-title type="subtitle" size="3">Subtitle</vb-title>
-<vb-title type="subtitle" size="4">Subtitle</vb-title>
-<vb-title type="subtitle" size="5">Subtitle</vb-title>
-<vb-title type="subtitle" size="6">Subtitle</vb-title>
+code.size.title = `\
+<vb-title size="1">Title 1</vb-title>
+<vb-title size="2">Title 2</vb-title>
+<vb-title size="3">Title 3 (default size)</vb-title>
+<vb-title size="4">Title 4</vb-title>
+<vb-title size="5">Title 5</vb-title>
+<vb-title size="6">Title 6</vb-title
 `
+
+code.size.subtitle = `\
+<vb-title type="subtitle" size="1">Subtitle 1</vb-title>
+<vb-title type="subtitle" size="2">Subtitle 2</vb-title>
+<vb-title type="subtitle" size="3">Subtitle 3</vb-title>
+<vb-title type="subtitle" size="4">Subtitle 4</vb-title>
+<vb-title type="subtitle" size="5">Subtitle 5 (default size)</vb-title>
+<vb-title type="subtitle" size="6">Subtitle 6</vb-title>
+`
+
 
 code.combinations = `\
 <vb-title>Title</vb-title>
--- examples/components/elements/Title.vue
+++ examples/components/elements/Title.vue
@@ -8,14 +8,16 @@
     </example-section-item>
 
     <example-section-item title="Sizes">
-      <demo :code="code.size">
+      <demo :code="code.size.title">
         <vb-title size="1">Title 1</vb-title>
         <vb-title size="2">Title 2</vb-title>
         <vb-title size="3">Title 3 (default size)</vb-title>
         <vb-title size="4">Title 4</vb-title>
         <vb-title size="5">Title 5</vb-title>
         <vb-title size="6">Title 6</vb-title>
+      </demo>
 
+      <demo :code="code.size.subtitle">
         <vb-title type="subtitle" size="1">Subtitle 1</vb-title>
         <vb-title type="subtitle" size="2">Subtitle 2</vb-title>
         <vb-title type="subtitle" size="3">Subtitle 3</vb-title>
