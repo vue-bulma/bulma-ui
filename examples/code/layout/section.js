7073ff51458a7b7448d379b85b2d788bd78dbe16
Guoxiao Cai <a304775438@aliyun.com>
2019-01-20 15:33:07 +0800

    fixup: remove example Hero in Section. Add description

--- examples/code/layout/section.js
+++ examples/code/layout/section.js
@@ -2,35 +2,41 @@ let code = {}
 
 code.basic = `\
 <vb-section>
-  <vb-hero color="primary">
-    <vb-title>Section</vb-title>
-  </vb-hero>
+  <vb-container>
+    <vb-title>Section1</vb-title>
+    <vb-subtitle>
+      A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+    </vb-subtitle>
+  </vb-container>
 </vb-section>
 
 <vb-section>
-  <vb-hero color="info">
+  <vb-container>
     <vb-title>Section2</vb-title>
-  </vb-hero>
+    <vb-subtitle>
+      A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+    </vb-subtitle>
+  </vb-container>
 </vb-section>
 `
 
 code.sizes = `\
 <vb-section>
-  <vb-hero color="primary">
-    <vb-title>Default</vb-title>
-  </vb-hero>
+  <vb-container>
+    <vb-title>Default spacing</vb-title>
+  </vb-container>
 </vb-section>
 
 <vb-section size="medium">
-  <vb-hero color="info">
-    <vb-title>Medium</vb-title>
-  </vb-hero>
+  <vb-container>
+    <vb-title>Medium spacing</vb-title>
+  </vb-container>
 </vb-section>
 
 <vb-section size="large">
-  <vb-hero color="info">
-    <vb-title>Large</vb-title>
-  </vb-hero>
+  <vb-container>
+    <vb-title>Large spacing</vb-title>
+  </vb-container>
 </vb-section>
 `
 
--- examples/components/layout/Section.vue
+++ examples/components/layout/Section.vue
@@ -1,17 +1,27 @@
 <template>
   <vb-section>
     <example-section-item title="Basic">
+      <vb-content>
+        Use sections as direct children of <code>body</code>.
+      </vb-content>
+
       <demo :code="code.basic">
         <vb-section>
-          <vb-hero color="primary">
-            <vb-title>Section</vb-title>
-          </vb-hero>
+          <vb-container>
+            <vb-title>Section1</vb-title>
+            <vb-subtitle>
+              A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+            </vb-subtitle>
+          </vb-container>
         </vb-section>
 
         <vb-section>
-          <vb-hero color="info">
+          <vb-container>
             <vb-title>Section2</vb-title>
-          </vb-hero>
+            <vb-subtitle>
+              A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+            </vb-subtitle>
+          </vb-container>
         </vb-section>
       </demo>
     </example-section-item>
@@ -23,25 +33,25 @@
 
       <demo :code="code.sizes">
         <vb-section>
-          <vb-hero color="primary">
-            <vb-title>Default</vb-title>
-          </vb-hero>
+          <vb-container>
+            <vb-title>Default spacing</vb-title>
+          </vb-container>
         </vb-section>
 
         <hr>
 
         <vb-section size="medium">
-          <vb-hero color="info">
-            <vb-title>Medium</vb-title>
-          </vb-hero>
+          <vb-container>
+            <vb-title>Medium spacing</vb-title>
+          </vb-container>
         </vb-section>
 
         <hr>
 
         <vb-section size="large">
-          <vb-hero color="warning">
-            <vb-title>Large</vb-title>
-          </vb-hero>
+          <vb-container>
+            <vb-title>Large spacing</vb-title>
+          </vb-container>
         </vb-section>
       </demo>
     </example-section-item>
